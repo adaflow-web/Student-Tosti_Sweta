6a2ffebacd70ce058b720e97bb85e261652974cb
sweta <swetat007@protonmail.com>
2023-01-06 13:49:41 +0100

    Remove Last index content in a collection by using removeChild

---
+++ crud-JS/getrid.js
@@ -0,0 +1,23 @@
+const content = document.getElementById("content");
+const myText = document.getElementById("myText");
+const addButton = document.getElementById("addButton");
+const removeButton = document.getElementById("removeButton");
+
+function addPara(){
+    const para = document.createElement("p");
+    para.className= "newClass";
+    para.innerText = myText.value;
+    content.appendChild(para);
+}
+addButton.addEventListener("click", addPara);
+
+
+function removePara(){
+    const para = document.getElementsByClassName("newClass");  
+    if(para.length > 0){
+        content.removeChild(para[(para.length) - 1 ]);
+    }
+
+}
+console.log("Htlll");
+removeButton.addEventListener("click", removePara);
