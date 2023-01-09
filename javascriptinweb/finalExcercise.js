9b586ac070fa7c107c91a45001660cc9105abb06
sweta <swetat007@protonmail.com>
2023-01-09 15:07:52 +0100

    tettst

--- javascriptinweb/finalExcercise.js
+++ javascriptinweb/finalExcercise.js
@@ -4,15 +4,23 @@ const removeItem = document.getElementById("removeItem");
 const myText = document.getElementById("myText");
 const clearAll = document.getElementById("clearAll");
 
+let storageList = [];
+
 function addContent(){
     const paragraph = document.createElement("p");
     paragraph.innerText = myText.value;
     localStorage.setItem("Item", paragraph);
     content.appendChild(paragraph);
     paragraph.className = "newClass";
-    if (myText.value.length != 0){
-        myText.value = null;  
-    }             
+    
+    function store(){
+        if (myText.value.length != 0){
+            storageList.push(myText.value);
+            localStorage.setItem((storageList.length), myText.value );
+            // myText.value = null ;
+        }  
+    }
+    store(); 
 }
 
 addItem.addEventListener("click", addContent);
@@ -21,13 +29,16 @@ function removeContent(){
     const paragraph = document.getElementsByClassName("newClass");
     if (paragraph.length >= 1){
         content.removeChild(paragraph[(paragraph.length) - 1 ]);
+        localStorage.removeItem((paragraph.length - 1));
     }
+
 }
 
 removeItem.addEventListener("click", removeContent);
 
 function empty(){
     document.getElementById("content").innerHTML = "";
+    localStorage.clearAll;
 }
 
 clearAll.addEventListener("click", empty)
