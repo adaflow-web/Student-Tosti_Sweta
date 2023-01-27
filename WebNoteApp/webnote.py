dde5edb84834ccac376c6a90bcfc5fad9ba5d152
sweta <swetat007@protonmail.com>
2023-01-27 07:09:56 +0100

    change code for username.html which works & we get Welcome message for user after submit the name.

--- WebNoteApp/webnote.py
+++ WebNoteApp/webnote.py
@@ -1,12 +1,19 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask("webnote")
-
-
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+@app.route("/username", methods=[ "GET", "POST" ])
+def username():
+    return render_template("username.html", user_name = request.args["user_name"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
 
