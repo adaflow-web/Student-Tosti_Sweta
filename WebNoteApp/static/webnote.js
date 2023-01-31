f10c97ae31d843223a87715e21b3abdc708a2844
sweta <swetat007@protonmail.com>
2023-01-31 05:49:48 +0100

    null problem solved in prompt

--- WebNoteApp/static/webnote.js
+++ WebNoteApp/static/webnote.js
@@ -1,27 +1,24 @@
-// function storename(){
-//     //retrieving data
-//     const username = document.getElementById("username").value;
-//     //storing data
-//     
 
-// }
 function onloadFun (){
     if ( localStorage.length > 0 ){
         const user = localStorage.getItem("username");
-        return document.getElementById("username").innerHTML = user + "! Welcome back .";
-   
+        return document.getElementById("username").innerHTML = user + " Welcome back !";
+        
     }
     else{ 
         let userName = prompt("Please enter your name","" );
-        if(userName != "" ){
-           // document.getElementById("username").innerHTML = "Hello, " + userName; 
+        if(userName != null ){
+            document.getElementById("username").innerHTML = "Hello, " + userName; 
             localStorage.setItem("username", userName);
         }
-        
+        else{
+            return false;
+        }      
     }
-
-    location.href="/";    
+       
 }
+
+
 
 
 
