4f12c85ea0e1d021becd2be4fbac136ba6182184
sweta <swetat007@protonmail.com>
2023-02-20 12:20:41 +0100

    value 'welcome null' avoided

--- Back-end/flaskfiles/finalex/static/webnote.js
+++ Back-end/flaskfiles/finalex/static/webnote.js
@@ -1,19 +1,25 @@
 function onloadFun() {
-    if (localStorage.length > 0) {
-        const user = localStorage.getItem("username");
-        return document.getElementById("username").innerHTML = " Welcome " + user;    
-    }
+    if (localStorage.length > 0){
+        for (var key in localStorage){
+            if (key == "username"){
+                const user = localStorage.getItem("username");
+                return document.getElementById("username").innerHTML = " Welcome " + user;    
+            }
+            else{
+                let user = prompt("Please enter your name", "");
+                document.getElementById("username").innerHTML = "Hello " + user;
+            }
+        }
+    }    
     else {
-        let user = prompt("Please enter your name", "");
-
+        let user = prompt("Please enter your name", ""); 
         while(user == ""){
             user = prompt("Please enter your name", "");    
-        }
-        
+        }       
         if(user != null){
             document.getElementById("username").innerHTML = "Hello " + user;
             localStorage.setItem("username", user);
         }       
-    }
+    }   
 }
 window.onload = onloadFun;
